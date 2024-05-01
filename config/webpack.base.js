3204fbd0e3b446837d87f18d0ddf0c5218cdc4d3
Kirisame <gensky377@gmail.com>
2024-05-01 18:52:43 +0900

    chore: fix

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -42,7 +42,7 @@ module.exports = {
     },
   },
   plugins: [
-    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
+    new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en/),
     new ForkTsCheckerWebpackPlugin(),
   ],
 };
