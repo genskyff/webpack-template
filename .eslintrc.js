fbd1298a2bdb751212a89043dce65fbe64f1ec2e
Kirisame <gensky377@gmail.com>
2024-08-02 00:45:39 +0900

    lint: fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,12 @@
 
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
   ],
-  files: ["src"],
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
